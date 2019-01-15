f50d1d58fea6115e125dad1fcb62cc427fd76b93
github username <github email address>
2019-01-15 12:56:01 -0500

    add content and tests

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,29 @@
+class Ingredient
 
+  attr_reader :ing_name
+
+  @@all = []
+
+  def initialize(ing_name)
+    @ing_name = ing_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def allergens
+    Allergen.all.select {|all| all.ingredient == self}
+  end
+
+  def allergencount
+    self.allergens.count
+  end
+
+  def self.most_common_allergen
+    most_all = Allergen.all.max_by {|x| x.ingredient.allergencount}
+    most_all.ingredient
+  end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,32 @@
 require_relative '../config/environment.rb'
 
+cris = User.new("Cristian")
+cass = User.new("Cassidy")
+prince = User.new("Prince")
+helen = User.new("Helen")
+
+las = Recipe.new("Lasagna")
+stk = Recipe.new("Steak")
+
+chz = Ingredient.new("Cheese")
+bf = Ingredient.new("Beef")
+
+stk_rc = RecipeCard.new(cris, stk, 5, "5")
+stk_rc_2 = RecipeCard.new(helen, stk, 2, "4")
+stk_rc_3 = RecipeCard.new(prince, stk, 7, "4")
+las_rc = RecipeCard.new(cass, las, 5, "3")
+las_rc_2 = RecipeCard.new(cris, las, 3, "4")
+
+las_ri = RecipeIngredient.new(chz, las)
+stk_ri = RecipeIngredient.new(bf, stk)
+stk_ri_2 = RecipeIngredient.new(bf, las)
+
+meat_all = Allergen.new(cass, bf)
+chz_all = Allergen.new(cris, chz)
+meat_all_2 = Allergen.new(cris, bf)
+chz_all_2 = Allergen.new(prince, chz)
+chz_all_3 = Allergen.new(helen, chz)
+
+ingredient_arr = [chz, bf]
+
 binding.pry
